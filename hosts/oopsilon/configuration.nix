77c8c9c023b790d3c6d1b7953ae3fc2a3c2292e3
Jack Bashford <jack@jackbashford.com>
2024-12-24 15:00:09 +1100

    zsh config, reorganise packages, add skhd and yabai services

--- hosts/oopsilon/configuration.nix
+++ hosts/oopsilon/configuration.nix
@@ -6,23 +6,76 @@
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
-  environment.systemPackages = [
-    pkgs.nixfmt-rfc-style
-    pkgs.nil
-  ];
+  environment.pathsToLink = [ "/share/zsh" ];
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
-
-  security.pam.enableSudoTouchIdAuth = true;
 
   system.keyboard = {
     enableKeyMapping = true;
     remapCapsLockToEscape = true;
   };
 
-  users.users.jackbashford.home = "/Users/jackbashford";
+  users.users.jackbashford = {
+    home = "/Users/jackbashford";
+    packages = [
+      pkgs.nixfmt-rfc-style
+      pkgs.nil
+    ];
+  };
+
+  security.pam.enableSudoTouchIdAuth = true;
+
+  services.yabai = {
+    enable = true;
+    enableScriptingAddition = false;
+    config = {
+      layout = "bsp";
+      window_placement = "second_child";
+      focus_follows_mouse = "autoraise";
+      mouse_follows_focus = "off";
+      top_padding = 8;
+      bottom_padding = 8;
+      left_padding = 4;
+      right_padding = 4;
+      window_gap = 2;
+    };
+  };
+
+  services.skhd = {
+    enable = true;
+    skhdConfig = ''
+      shift + alt - y: yabai --restart-service
+      alt - y: yabai --stop-service
+
+      alt - t: kitty --single-instance -d ~
+      shift + alt - d: discord
+
+      shift + alt - h: yabai -m window --warp west
+      shift + alt - j: yabai -m window --warp south
+      shift + alt - k: yabai -m window --warp north
+      shift + alt - l: yabai -m window --warp east
+
+      alt - 1: yabai -m space --focus 1
+      alt - 2: yabai -m space --focus 2
+      alt - 3: yabai -m space --focus 3
+      alt - 4: yabai -m space --focus 4
+      alt - 5: yabai -m space --focus 5
+      alt - 6: yabai -m space --focus 6
+      alt - 7: yabai -m space --focus 7
+
+      shift + alt - 1: yabai -m window --space 1
+      shift + alt - 2: yabai -m window --space 2
+      shift + alt - 3: yabai -m window --space 3
+      shift + alt - 4: yabai -m window --space 4
+      shift + alt - 5: yabai -m window --space 5
+      shift + alt - 6: yabai -m window --space 6
+      shift + alt - 7: yabai -m window --space 7
+
+      alt - f : yabai -m window --toggle zoom-fullscreen
+    '';
+  };
 
   home-manager.backupFileExtension = "bak";
   home-manager.users.jackbashford = {
@@ -30,6 +83,15 @@
       zsh = {
         enable = true;
         autocd = true;
+        autosuggestion.enable = true;
+        enableCompletion = true;
+        syntaxHighlighting = {
+          enable = true;
+          highlighters = [
+            "brackets"
+            "cursor"
+          ];
+        };
 
         history = {
           append = true;
@@ -38,11 +100,15 @@
           size = 1000000;
         };
 
-        initExtra = "setopt INC_APPEND_HISTORY";
+        initExtra = ''
+          setopt INC_APPEND_HISTORY
+          bindkey "^[[3~" delete-char
+        '';
 
         shellAliases = {
           j = "zellij";
           l = "lsd";
+          ls = "lsd -A";
         };
       };
 
