7154eb927b6239b9fc4bf7be173693db493ff39d
Jack Bashford <jack@jackbashford.com>
2024-12-29 15:51:00 +1100

    gleam, spacebar (disabled atm)

--- hosts/oopsilon/configuration.nix
+++ hosts/oopsilon/configuration.nix
@@ -17,11 +17,19 @@
     remapCapsLockToEscape = true;
   };
 
+  system.defaults.NSGlobalDomain._HIHideMenuBar = false;
+
   users.users.jackbashford = {
     home = "/Users/jackbashford";
     packages = [
-      pkgs.nixfmt-rfc-style
       pkgs.nil
+      pkgs.nixfmt-rfc-style
+
+      pkgs.git
+
+      pkgs.gleam
+      pkgs.erlang
+
       pkgs.typst
       pkgs.tinymist
       pkgs.typstyle
@@ -53,13 +61,17 @@
       yabai -m rule --add app="^System Settings$" manage=off
       yabai -m rule --add app="^1Password$" manage=off
       yabai -m rule --add app="^Raycast$" manage=off
+
+      yabai -m signal --add app='^Ghostty$' event=window_created action='yabai -m space --layout bsp'
+      yabai -m signal --add app='^Ghostty$' event=window_destroyed action='yabai -m space --layout bsp'
     '';
+    # yabai -m config external_bar all:26:0
   };
 
   services.skhd = {
     enable = true;
     skhdConfig = ''
-      alt - t: kitty --single-instance -d ~
+      alt - t: open -a /Applications/Ghostty.app
 
       alt - y: yabai -m window --toggle float
 
@@ -86,6 +98,47 @@
 
       alt - f : yabai -m window --toggle zoom-fullscreen
     '';
+  };
+
+  services.spacebar = {
+    enable = false;
+    package = pkgs.spacebar;
+    config = {
+      position = "top";
+      display = "main";
+      height = 26;
+      title = "on";
+      spaces = "on";
+      clock = "on";
+      power = "on";
+      padding_left = 20;
+      padding_right = 20;
+      spacing_left = 25;
+      spacing_right = 15;
+      # text_font = ''"Menlo:Regular:12.0"'';
+      # icon_font = ''"Font Awesome 5 Free:Solid:12.0"'';
+      background_color = "0xff202020";
+      foreground_color = "0xffa8a8a8";
+      power_icon_color = "0xffcd950c";
+      battery_icon_color = "0xffd75f5f";
+      dnd_icon_color = "0xffa8a8a8";
+      clock_icon_color = "0xffa8a8a8";
+      power_icon_strip = " ";
+      space_icon = "•";
+      space_icon_strip = "1 2 3 4 5 6 7 8 9 10";
+      spaces_for_all_displays = "on";
+      display_separator = "on";
+      display_separator_icon = "";
+      space_icon_color = "0xff458588";
+      space_icon_color_secondary = "0xff78c4d4";
+      space_icon_color_tertiary = "0xfffff9b0";
+      clock_icon = "";
+      dnd_icon = "";
+      clock_format = ''"%d/%m/%y %R"'';
+      right_shell = "on";
+      right_shell_icon = "";
+      right_shell_command = "whoami";
+    };
   };
 
   # Prevents slow shell startup, we already compinit per-user,
